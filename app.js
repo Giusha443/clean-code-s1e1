d6a4e6b5d79521de6bba1946d02911f47d175991
Giusha443 <giorgi.jajanidze2003@gmail.com>
2024-12-14 19:36:10 +0400

    fix: add semantic tags and media alternatives according to rules 1.1 and 1.2 from html-and-css-extended.md

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="delete button";
     deleteButtonImg.src="./remove.svg";
+    deleteButtonImg.alt="delete-icon";
     deleteButtonImg.classList="delete-image";
     deleteButton.appendChild(deleteButtonImg);
 
